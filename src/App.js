b4d9f1b905e0bb0ec73a2824efb1d9cd7c99a6bd
Delwin Best <mail@delwinbest.com>
2021-06-14 17:02:28 +0900

    base styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import { useDispatch } from "react-redux";
+import React from "react";
+
+import "./sass/index.scss";
+import { useSelector, useDispatch } from "react-redux";
 import * as actionTypes from "./store/actions/actionTypes";
 
 function App() {
   const dispatch = useDispatch();
+  const team = useSelector((state) => state.team);
 
   const addMemberHandler = (username) => {
     dispatch({ type: actionTypes.TEAM_ADD_MEMBER, username: "DDDDD" });
   };
+
+  const removeMemberHandler = (username) => {
+    dispatch({ type: actionTypes.TEAM_REMOVE_MEMBER, username: username });
+  };
+
+  const teamList = team.teamMembers.map((employee) => {
+    return <div onClick={() => removeMemberHandler(employee)}>{employee}</div>;
+  });
+
   return (
-    <div>
-      <header>Learn React</header>
-      <button onClick={addMemberHandler}>Add User</button>
-    </div>
+    <React.Fragment>
+      <div className="bg__header"></div>
+      <div className="mainview-container">
+        <header className="header"></header>
+        <div className="maincontent">
+          <div className="card" style={{ width: "20rem" }}>
+            <h2>Team Members</h2>
+            {teamList}
+            <button onClick={addMemberHandler}>Click Me</button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
--- src/store/reducers/team.js
+++ src/store/reducers/team.js
@@ -11,10 +11,24 @@ const addMember = (state, action) => {
   };
 };
 
+const removeMember = (state, action) => {
+  const teamMembers = state.teamMembers
+    .filter((employee) => employee !== action.username)
+    .map((employee) => {
+      return employee;
+    });
+  return {
+    ...state,
+    teamMembers: teamMembers,
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.TEAM_ADD_MEMBER:
       return addMember(state, action);
+    case actionTypes.TEAM_REMOVE_MEMBER:
+      return removeMember(state, action);
     default:
       return state;
   }
